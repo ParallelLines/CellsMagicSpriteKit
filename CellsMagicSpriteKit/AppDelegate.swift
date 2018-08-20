0e81f2cc55c2380c1cc6f3416579859fe3d951c6
ParallelLines <tishkane@yandex.ru>
2018-08-20 20:44:40 +0300

    Initial Commit

---
+++ CellsMagicSpriteKit/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CellsMagicSpriteKit
+//
+//  Created by Tatyana Khabirova on 20/08/2018.
+//  Copyright © 2018 Tatyana Khabirova. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
